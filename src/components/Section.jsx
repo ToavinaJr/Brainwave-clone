abc400448f2ded0c659c0451c2dc1ce3fc755372
Toavina Sylvianno <toavinaslvianno14@gmail.com>
2024-06-30 03:18:29 +0300

    Final release

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,4 @@
-import SectionSvg from "../../public/assets/svg/SectionSvg";
+import SectionSvg from "../assets/svg/SectionSvg";
 
 const Section = ({
   className,
@@ -10,7 +10,7 @@ const Section = ({
 }) => (
   <div
     id={id}
-    className={`relative 
+    className={`relative
     ${
       customPaddings ||
       `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
@@ -24,7 +24,7 @@ const Section = ({
     {crosses && (
       <>
         <div
-          className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+          className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25  bg-stroke-1 ${
             crossesOffset && crossesOffset
           } pointer-events-none lg:block xl:left-10 right-10`}
         />
